570560abf6c7309738830aaa454ac629913e4896
JLPBoss <jettpennington@gmail.com>
2023-10-23 08:45:21 -0400

    added custome file extention and pthoun run comand

--- namekata.py
+++ namekata.py
@@ -4,4 +4,7 @@ kata = input("Enter Kata Name: ")
 kata = kata.lower()
 kata = re.sub(r'[^a-zA-Z0-9\s]+', '', kata)
 kata = kata.replace(" ", "-", -1)
-print(kata + ".js")
+katatype = "." + input("Enter file Extention")
+print(kata + katatype)
+
+#python namekata.py
