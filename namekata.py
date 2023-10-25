772fe8f641ee47bf327189d0e7ab67649332035c
JLPBoss <jettpennington@gmail.com>
2023-10-25 09:32:17 -0400

    added spaceing on entering extention

--- namekata.py
+++ namekata.py
@@ -4,7 +4,7 @@ kata = input("Enter Kata Name: ")
 kata = kata.lower()
 kata = re.sub(r'[^a-zA-Z0-9\s]+', '', kata)
 kata = kata.replace(" ", "-", -1)
-katatype = "." + input("Enter file Extention")
+katatype = "." + input("Enter file Extention: ")
 print(kata + katatype)
 
 #python namekata.py
